f0d740429c93fd0334d08351ff9e9820dd30516b
gfbsant <gfbsant@gmail.com>
2025-12-01 23:42:56 -0300

    Remove unused ResidentModel and related generated files

--- lib/features/apartments/data/models/apartment_model.dart
+++ lib/features/apartments/data/models/apartment_model.dart
@@ -2,12 +2,7 @@
 // ignore_for_file: overridden_fields
 
 import 'package:json_annotation/json_annotation.dart';
-
-<<<<<<< HEAD
-import '../../../residents/data/resident_model.dart';
-=======
 import '../../../residents/data/models/resident_model.dart';
->>>>>>> 1855d78 (Implement resident management features)
 import '../../../residents/domain/entities/resident_entity.dart';
 import '../../domain/entities/apartment_entity.dart';
 
